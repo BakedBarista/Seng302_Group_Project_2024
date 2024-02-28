b6282016613a76fe3cedd0fce8ac9242b042a0ee
Benjamin Davies <bentendavies@gmail.com>
2024-02-28 13:39:30 +1300

    Split test

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/UserServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/UserServiceTest.java
@@ -12,19 +12,31 @@ import org.springframework.context.annotation.Import;
 @Import(GardenUserService.class)
 public class UserServiceTest {
 
-   @Autowired
-   private GardenUserService userService;
+    @Autowired
+    private GardenUserService userService;
 
-    // simple test to chcek if inserting information into the database works
+    /**
+     * Simple test to check if inserting information into the database works
+     */
     @Test
     public void insertingUser() {
-        GardenUser gardenUser = userService.addUser(new GardenUser("fname", "lname", "email", "address", "password", "dob"));
+        GardenUser gardenUser = userService
+                .addUser(new GardenUser("fname", "lname", "email", "address", "password", "dob"));
         Assertions.assertEquals(gardenUser.getFname(), "fname");
         Assertions.assertEquals(gardenUser.getLname(), "lname");
         Assertions.assertEquals(gardenUser.getEmail(), "email");
         Assertions.assertEquals(gardenUser.getDOB(), "dob");
+        Assertions.assertEquals(gardenUser.getAddress(), "address");
+    }
+
+    /**
+     * Check that we can check a user's password
+     */
+    @Test
+    public void checkPassword() {
+        GardenUser gardenUser = userService
+                .addUser(new GardenUser("fname", "lname", "email", "address", "password", "dob"));
         Assertions.assertTrue(gardenUser.checkPassword("password"));
         Assertions.assertFalse(gardenUser.checkPassword("incorrect password"));
-        Assertions.assertEquals(gardenUser.getAddress(), "address");
     }
 }
