8a85e761c18ee0f028825d740fb25487f6242348
Benjamin Davies <bda71@uclive.ac.nz>
2024-03-01 15:46:48 +1300

    Format date correctly

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/users/UserController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/users/UserController.java
@@ -1,6 +1,8 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller.users;
 
 import java.security.Principal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,7 +34,12 @@ public class UserController {
         model.addAttribute("fname", user.getFname());
         model.addAttribute("lname", user.getLname());
         model.addAttribute("email", user.getEmail());
-        model.addAttribute("dob", user.getDOB());
+
+        if (user.getLname() != null) {
+            LocalDate dob = LocalDate.parse(user.getDOB());
+            String dobString = dob.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            model.addAttribute("dob", dobString);
+        }
 
         return "users/user";
     }
