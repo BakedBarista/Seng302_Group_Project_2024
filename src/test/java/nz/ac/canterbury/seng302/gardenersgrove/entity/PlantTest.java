db98c68d589237d10901beeed58a2e66c415415c
Ryan Scofield <rsc104@uclive.ac.nz>
2024-03-01 16:25:39 +1300

    Wrote placeholder methods for junit tests for Plant class

---
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/entity/PlantTest.java
@@ -0,0 +1,120 @@
+package nz.ac.canterbury.seng302.gardenersgrove.entity;
+
+public class PlantTest {
+    public void SetName_NameIsPlant_Successful() {
+
+    }
+
+    public void SetName_NameHasSpace_Successful() {
+
+    }
+
+    public void SetName_NameHasSpaces_Successful() {
+
+    }
+
+    public void SetName_NameHasCommas_Successful() {
+
+    }
+
+    public void SetName_NameHasHyphens_Successful() {
+
+    }
+
+    public void SetName_NameHasApostrophes_Successful() {
+
+    }
+
+    public void SetName_NameHasNumbers_Successful() {
+
+    }
+
+    public void SetName_NameHasExclamationMark_DoesNotAccept() {
+
+    }
+
+    public void SetName_NameHasHash_DoesNotAccept() {
+
+    }
+
+    public void SetName_Null_NameCannotBeNull () {
+
+    }
+
+    public void SetCount_Null_Successful() {
+
+    }
+
+    public void SetCount_Zero_Successful() {
+
+    }
+
+    public void SetCount_One_Successful() {
+
+    }
+
+    public void SetCount_NegativeOne_Successful() {
+
+    }
+
+    public void SetCount_DotAsDecimalPlace_Successful() {
+
+    }
+
+    public void SetCount_CommaAsDecimalPlace_Successful() {
+
+    }
+
+    public void SetCount_IntegerWithNonNumericChar_DoesNotAccept() {
+
+    }
+
+    public void SetCount_NonNumericChar_DoesNotAccept() {
+
+    }
+
+    public void SetCount_DoubleDotAsDecimalPlace_DoesNotAccept() {
+
+    }
+
+    public void SetCount_DoubleCommaAsDecimalPlace_DoesNotAccept() {
+
+    }
+
+    public void SetDescription_Null_Successful() {
+
+    }
+
+    public void SetDescription_AlphanumericChars_Successful() {
+
+    }
+
+    public void SetDescription_AlphanumericCharsAndSpecialChars_Successful() {
+
+    }
+
+    public void SetDescription_FiveHundredAndElevenChars_Successful() {
+
+    }
+
+    public void SetDescription_FiveHundredAndTwelveChars_DoesNotAccept() {
+
+    }
+
+    public void SetPlantedDate_Null_Successful() {
+
+    }
+
+    public void SetPlantedDate_DDMMYYYY_Successful() {
+
+    }
+
+    public void SetPlantedDate_MMDDYYYY_DoesNotAccept() {
+
+    }
+
+    public void SetPlantedDate_YYYYDDMM_DoesNotAccept() {
+
+    }
+
+}
