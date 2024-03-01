b2fea584d99d79f22ae573bf581bc5c512d77331
Mfr91 <mfr91@uclive.ac.nz>
2024-03-01 14:29:40 +1300

    got rid of homepage.html and added logout button on the general fragment instead

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/security/SecurityConfiguration.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/security/SecurityConfiguration.java
@@ -76,8 +76,11 @@ public class SecurityConfiguration {
         // Define logging out, a POST "/logout" endpoint now exists under the hood,
         // redirect to "/login", invalidate session and remove cookie
         http.logout(
-                logout -> logout.logoutUrl("/users/logout").logoutSuccessUrl("/users/login")
-                        .invalidateHttpSession(true).deleteCookies("JSESSIONID"));
+                logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/users/logout"))
+                        .logoutSuccessUrl("/users/login")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID"));
         return http.build();
 
     }
