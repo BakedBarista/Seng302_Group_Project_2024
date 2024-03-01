0ff820857db57f19d1ce49144d18df26e9e94183
Carl Chen <zch66@uclive.ac.nz>
2024-03-01 15:32:29 +1300

    Updated plant entity and plantService for saving and getting imagePath.

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PlantController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/PlantController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 // TODO: THIS WHOLE FILE NEEDS TO BE UPDATED
@@ -25,11 +26,12 @@ public class PlantController {
         this.plantService = plantService;
     }
 
-    @GetMapping("/gardens/{id}/add-plant")
+    @GetMapping("/gardens/{id}/add_plant")
     public String form(@RequestParam(name="name", required = false, defaultValue = "") String name,
                        @RequestParam(name="count", required = false, defaultValue = "") String count,
                        @RequestParam(name="description", required = false, defaultValue = "") String description,
                        @RequestParam(name="plantedDate", required = false, defaultValue = "") String plantedDate,
+                       @PathVariable(name="id") Long id,
                        Model model) {
         logger.info("GET /gardens/${id}/add-plant - display the new plant form");
         return "gardens/createGarden"; // TODO: Add the plant form
--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/UploadController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/UploadController.java
@@ -1,6 +1,7 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
 
+import nz.ac.canterbury.seng302.gardenersgrove.service.PlantService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,41 +13,36 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
 
 /**
- * Controller class for uploading images
+ * Controller for handling file uploads
  */
 @Controller
 public class UploadController {
     public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads";
-    private static final List<String> ALLOWED_EXTENSIONS = Arrays.asList("png", "jpg", "jpeg", "svg");
-    @GetMapping("/uploadimage")
-    public String displayUploadForm() {
+    private final PlantService plantService;
+
+    public UploadController(PlantService plantService) {this.plantService = plantService;}
+    /**
+     * Displays the file upload page
+     */
+    @GetMapping("/gardens/{id}/plants/uploadImage")
+    public String displayUploadForm(@RequestParam("id")String id,
+                                    Model model) {
+        model.addAttribute("id", id);
         return "images/uploadPage";
     }
 
-    @PostMapping("/upload")
+    @PostMapping("/gardens/{id}/plants/uploadImage")
     public String uploadImage(Model model,
-                              @RequestParam("image") MultipartFile file) throws IOException {
-        if (file.isEmpty()) {
-            model.addAttribute("message", "Please select a file to upload");
-            return "images/uploadPage";
-        }
-        String originalFilename = file.getOriginalFilename();
-        String fileExtension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1).toLowerCase();
-
-        if(!ALLOWED_EXTENSIONS.contains(fileExtension)) {
-            model.addAttribute("fileTypeError", "Only PNG,JPG, and SVG files are allowed");
-            return "images/uploadPage";
-        }
-
-        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, originalFilename);
-        Files.write(fileNameAndPath,file.getBytes());
-
-        model.addAttribute("message","File uploaded successfully");
-        model.addAttribute("msg", "Uploaded images: " + originalFilename);
-        return "images/uploadPage";
-    } //TODO: show error message on page
+                              @RequestParam("image")MultipartFile file,
+                              @RequestParam("id") Long id) throws IOException {
+        StringBuilder fileName = new StringBuilder();
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
+        fileName.append(file.getOriginalFilename());
+        Files.write(fileNameAndPath, file.getBytes());
+        plantService.setPlantImage(id,fileNameAndPath.toString());
+        model.addAttribute("msg", "Uploaded images: " + fileName.toString());
+        return "imageupload/index";
+    }
 }
