d21ccc78dfe0f2e3a577c67eb96a92445ce8792c
Carl Chen <zch66@uclive.ac.nz>
2024-03-03 22:44:09 +1300

    Updated error checking and endpoints

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/UploadController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/UploadController.java
@@ -5,6 +5,7 @@ import nz.ac.canterbury.seng302.gardenersgrove.service.PlantService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 /**
  * Controller for handling file uploads
@@ -26,23 +28,46 @@ public class UploadController {
     /**
      * Displays the file upload page
      */
-    @GetMapping("/gardens/{id}/plants/uploadImage")
-    public String displayUploadForm(@RequestParam("id")String id,
+    @GetMapping("/gardens/{gardenId}/plants/{plantId}/uploadImage")
+    public String displayUploadForm(@PathVariable("gardenId")Long gardenId,
+                                    @PathVariable("plantId")Long plantId,
                                     Model model) {
-        model.addAttribute("id", id);
+        model.addAttribute("gardenId", gardenId);
+        model.addAttribute("plantId",plantId);
         return "images/uploadPage";
     }
 
-    @PostMapping("/gardens/{id}/plants/uploadImage")
+    @PostMapping("/gardens/{gardenId}/plants/{plantId}/uploadImage")
     public String uploadImage(Model model,
                               @RequestParam("image")MultipartFile file,
-                              @RequestParam("id") Long id) throws IOException {
-        StringBuilder fileName = new StringBuilder();
-        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
-        fileName.append(file.getOriginalFilename());
-        Files.write(fileNameAndPath, file.getBytes());
-        plantService.setPlantImage(id,fileNameAndPath.toString());
-        model.addAttribute("msg", "Uploaded images: " + fileName.toString());
+                              @PathVariable("gardenId")Long gardenId,
+                              @PathVariable("plantId")Long plantId) throws IOException {
+        //Check if file is empty, used to display when first open the upload page
+        if(file.isEmpty()) {
+            model.addAttribute("error", "Please select a file to upload");
+            return "images/uploadPage";
+        }
+        //Check types
+        String contentType = file.getContentType();
+        if (contentType == null || !(contentType.equals("image/jpeg") || contentType.equals("image/png") || contentType.equals("image/svg"))) {
+            model.addAttribute("fileTypeError", "Only JPG, PNG, and SVG files are allowed");
+            return "images/uploadPage";
+        }
+        //Check size
+        if (file.getSize() > 10 * 1024 * 1024) { // 10MB limit
+            model.addAttribute("fileSizeError", "File size exceeds the limit of 10MB");
+            return "images/uploadPage";
+        }
+        //Generates a unique filename for the saved image to avoid future conflict
+        try {
+            String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+            Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, fileName);
+            Files.write(fileNameAndPath, file.getBytes());
+            plantService.setPlantImage(plantId,fileNameAndPath.toString());
+            model.addAttribute("msg","Uploaded image: " + fileName);
+        } catch (IOException e) {
+            model.addAttribute("UploadError", "Failed to upload image");
+        }
         return "imageupload/index";
     }
 }
