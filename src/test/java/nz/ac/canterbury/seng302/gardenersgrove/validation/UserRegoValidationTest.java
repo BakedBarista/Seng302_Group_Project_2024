212df1ce475c2d621d0d92b9b5c1abdf66498fea
Imogen Keeling <ike24@uclive.ac.nz>
2024-03-04 11:23:43 +1300

    refactored UserRegoValidationTest into the "validation" folder in tests

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/validation/UserRegoValidationTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/validation/UserRegoValidationTest.java
@@ -1,4 +1,4 @@
-package nz.ac.canterbury.seng302.gardenersgrove;
+package nz.ac.canterbury.seng302.gardenersgrove.validation;
 
 import nz.ac.canterbury.seng302.gardenersgrove.entity.GardenUser;
 import nz.ac.canterbury.seng302.gardenersgrove.repository.GardenUserRepository;
