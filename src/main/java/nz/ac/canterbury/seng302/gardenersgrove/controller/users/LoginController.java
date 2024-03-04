d060b7ca95a03de00652ecc32eb5089fe1fe515b
Imogen Keeling <ike24@uclive.ac.nz>
2024-03-04 14:41:18 +1300

    beginning Login edge case stuff

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/users/LoginController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/users/LoginController.java
@@ -1,13 +1,21 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import nz.ac.canterbury.seng302.gardenersgrove.service.GardenUserService;
 
 @Controller
 public class LoginController {
     private Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @Autowired
+    private GardenUserService gardenUserService;
+
 
     /**
      * Shows the user the login form
@@ -16,5 +24,14 @@ public class LoginController {
     public String login() {
         logger.info("GET /users/login");
         return "users/login";
+    }
+
+    public String submitLogin(
+            @RequestParam(name="email") String email,
+            @RequestParam(name="password") String password,
+            Model model) {
+        logger.info("POST /users/login");
+
+
     }
 }
