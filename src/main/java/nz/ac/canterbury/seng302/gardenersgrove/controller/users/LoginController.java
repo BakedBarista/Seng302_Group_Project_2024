6f771f528e339a1ea9c779cf3819b894e66a0e82
Benjamin Davies <bda71@uclive.ac.nz>
2024-03-06 14:24:03 +1300

    Remove unused code

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/users/LoginController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/users/LoginController.java
@@ -1,24 +1,15 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller.users;
 
-import nz.ac.canterbury.seng302.gardenersgrove.entity.GardenUser;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import nz.ac.canterbury.seng302.gardenersgrove.service.GardenUserService;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
     private Logger logger = LoggerFactory.getLogger(LoginController.class);
-
-    @Autowired
-    private GardenUserService gardenUserService;
-
 
     /**
      * Shows the user the login form
