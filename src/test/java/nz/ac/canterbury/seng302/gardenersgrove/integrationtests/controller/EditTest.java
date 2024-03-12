88d237911316419e463413757ab67b3653b0dfc5
Maxzi <mfr91@uclive.ac.nz>
2024-03-13 11:57:45 +1300

    Integration testing for register controller but it is failing to build

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integrationtests/controller/EditTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integrationtests/controller/EditTest.java
@@ -1,7 +1,7 @@
 package nz.ac.canterbury.seng302.gardenersgrove.integrationtests.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import nz.ac.canterbury.seng302.gardenersgrove.controller.users.RegisterController;
+import nz.ac.canterbury.seng302.gardenersgrove.controller.users.EditUserController;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.GardenUser;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenUserService;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,17 +15,21 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.ui.Model;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@WebMvcTest(controllers = RegisterController.class)
+@WebMvcTest(controllers = EditUserController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
-public class registerTest {
+public class EditTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -38,21 +42,38 @@ public class registerTest {
 
     private GardenUser gardenUser;
 
+    @Mock
+    private Authentication authentication;
+
+    @Mock
+    private Model model;
+
     @BeforeEach
     public void setup() {
-        gardenUser = new GardenUser("Maxzi", null, null, null, "password", null);
-
+        gardenUser = new GardenUser("$2Jane", "Dough", "jane@email.com", "home",
+                "P#ssw0rd", "01/01/1998");
     }
 
     @Test
-    public void UserController_RegisterUser_ReturnRegistered() throws Exception {
+    public void EditController_EditUser_ReturnEdited() throws Exception {
         // Mocking the behavior of userService.addUser to return the input gardenUser
-        given(gardenUserService.addUser(ArgumentMatchers.any())).willReturn(gardenUser);
+        given(gardenUserService.addUser(any())).willReturn(gardenUser);
+
+        // Mocking the behavior of authentication.getPrincipal() to return a Long value
+        given(authentication.getPrincipal()).willReturn(1L);
 
         // Performing a POST request to the controller endpoint
-        ResultActions response = mockMvc.perform(post("/users/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(gardenUser)));
+        ResultActions response = mockMvc.perform(post("/users/edit")
+                .param("fname", "Maxzi")
+                .param("lname", "Francisco")
+                .param("noLname", "false")
+                .param("email", "maxzi@gmail.com")
+                .param("address", "home")
+                .param("id", "1L")
+                .param("dob", "10/10/2000")
+                .param("profilePictureContentType", "jpg")
+                .param("profilePicture", "null")
+                .principal(authentication));
 
         // Verifying that the response status is 201 (created)
         response.andExpect(MockMvcResultMatchers.status().isCreated());
