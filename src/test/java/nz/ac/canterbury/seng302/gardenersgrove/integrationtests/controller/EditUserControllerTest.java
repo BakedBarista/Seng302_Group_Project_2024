aa4e8ddf0f3200444918f2dcbbc4a7c27d135202
liam ceelen-thomas <lce26@uclive.ac.nz>
2024-03-13 09:17:06 +1300

    one working test for controller, did unit tests for repository methods

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integrationtests/controller/EditUserControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integrationtests/controller/EditUserControllerTest.java
@@ -9,66 +9,61 @@ import nz.ac.canterbury.seng302.gardenersgrove.controller.users.EditUserControll
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.Authentication;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
-import java.util.ArrayList;
-
-import java.util.List;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest
+@WebMvcTest(controllers = EditUserController.class)
+@AutoConfigureMockMvc
 public class EditUserControllerTest{
 
     @Autowired
     private MockMvc mockMvc;
 
-    List<GardenUser> databaseUsers = new ArrayList<>();
+    @MockBean
+    private GardenUserService gardenUserService;
 
-    GardenUser mockUser;
+
 
     @BeforeEach
     void setup() {
-
-        GardenUserRepository mockGardenUserRepository = Mockito.mock(GardenUserRepository.class);
-        GardenUserService gardenUserService = new GardenUserService(mockGardenUserRepository);
         EditUserController editUserController = new EditUserController();
-
-
-       // For testing getting users by ID
-       GardenUser mockUser = new GardenUser("liam", "ceelen", "liam@gmail.com",  "15", "liamspassword1?", "05/05/2003");
-       Mockito.when(gardenUserService.getUser()).thenReturn(List.of(mockUser));
-
-       // For testing getting ALL gardens from the database
-       Mockito.when(mockGardenUserRepository.findAll()).thenReturn(databaseUsers);
-
-
-       this.mockMvc = MockMvcBuilders.standaloneSetup(editUserController).build();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(editUserController).build();
    }
 
 
-    @Test
+
+   // simple test when no deatils are supplied a 400 is created 
+   @Test
    public void allFieldsNullTest() {
+        boolean noLname = false;
        try {
            mockMvc.perform(post("/users/edit")
                    .param("fname", (String) null)
                    .param("lname", (String) null)
+                   .param("noLname", String.valueOf(noLname))
                    .param("email", (String) null)
                    .param("address", (String) null)
-                   .param("email", (String) null)
-                   .param("dob", (String) null))
-                   .andExpectAll(
-                        model().attribute("incorrectFirstName", "First name cannot be empty and must only include letters, spaces,hyphens or apostrophes"),
-                        model().attribute("incorrectLastName", "First name cannot be empty and must only include letters, spaces,hyphens or apostrophes"),
-                        model().attribute("incorrectEmail", "Email address must be in the form ‘jane@doe.nz’")
-                   );
+                   .param("dob", (String) null)).andExpect(
+                    status().is(400)
+            );
        } catch (Exception e) {
            throw new RuntimeException(e);
        }
