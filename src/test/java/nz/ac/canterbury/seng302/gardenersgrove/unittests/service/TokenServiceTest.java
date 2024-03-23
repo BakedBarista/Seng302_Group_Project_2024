197462a986818b7184ae0559d0faeb35aaac2155
Benjamin Davies <bentendavies@gmail.com>
2024-03-23 15:46:10 +1300

    Add test for base64 token creation

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/service/TokenServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/service/TokenServiceTest.java
@@ -9,6 +9,15 @@ public class TokenServiceTest {
     private TokenService tokenService = new TokenService();
 
     @Test
+    public void testCreateAuthenticationToken_Is32Characters_OnlyContainsBase64Characters() {
+        int expectedLength = 32;
+        String token = tokenService.createAuthenticationToken();
+
+        Assertions.assertEquals(expectedLength, token.length());
+        Assertions.assertTrue(token.matches("^[A-Za-z0-9-_]+$"));
+    }
+
+    @Test
     public void testCreateEmailToken_IsSixDigits_OnlyContainsIntegers() {
         int expectedLength = 6;
         String token = tokenService.createEmailToken();
