0ed49502a7c0e2b8cab4bca0b3e79105f7a84fc6
Benjamin Davies <bentendavies@gmail.com>
2024-03-23 15:26:50 +1300

    Refactor token test a little

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/service/TokenServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/service/TokenServiceTest.java
@@ -1,11 +1,12 @@
-package nz.ac.canterbury.seng302.gardenersgrove.service;
+package nz.ac.canterbury.seng302.gardenersgrove.unittests.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class TokenServiceTest {
+import nz.ac.canterbury.seng302.gardenersgrove.service.TokenService;
 
-    TokenService tokenService = new TokenService();
+public class TokenServiceTest {
+    private TokenService tokenService = new TokenService();
 
     @Test
     public void testCreateEmailToken_IsSixDigits_OnlyContainsIntegers() {
@@ -13,6 +14,6 @@ public class TokenServiceTest {
         String token = tokenService.createEmailToken();
 
         Assertions.assertEquals(expectedLength, token.length());
-        Assertions.assertTrue(token.matches("\\d+"));
+        Assertions.assertTrue(token.matches("^\\d+$"));
     }
 }
