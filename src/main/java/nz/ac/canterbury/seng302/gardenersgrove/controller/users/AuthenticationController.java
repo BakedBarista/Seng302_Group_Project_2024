a6f496a78c68d7acaa934e4ae797c9ff4a44e110
Ryan Scofield <rsc104@uclive.ac.nz>
2024-03-24 20:11:59 +1300

    Worked on creating page for entering email token
    Finished html for basic token submitting and controllers
    made it so that after creating a new account, a token and time instant is added to the account and you are redirected to a token authentication page. If you type the correct token into this box, the token and time instant will be removed from your account

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/users/AuthenticationController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/users/AuthenticationController.java
@@ -1,5 +1,6 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller.users;
 
+import nz.ac.canterbury.seng302.gardenersgrove.entity.GardenUser;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -19,12 +21,55 @@ public class AuthenticationController {
     private GardenUserService userService;
 
 
-    @GetMapping("/users/authenticateEmail/{id}")
-    public String authenticateEmail(@PathVariable("id") Long userId, Model model) {
-        model.addAttribute("userId", userId);
-        return "/authentication/emailAuthentication";
+    /**
+     * Check to see if the user has a token, if they do take the user to the authentication page.
+     * This will be used after creating a new account - before being authenticated
+     * @param userId
+     * @param model
+     * @return authentication page
+     */
+    @GetMapping("/users/user/{userId}/authenticateEmail")
+    public String authenticateEmail(@PathVariable("userId") Long userId, Model model) {
+        // if the user has an authentication token
+        if (!(userService.getUserById(userId).getEmailValidationToken() == null)) {
+            model.addAttribute("userId", userId);
+            return "/authentication/emailAuthentication";
+        } else {
+            return "/error/404";
+        }
     }
 
-    @PostMapping("/users/validateAuthenticationToken/{givenToken}")
-    public String validateAuthenticationToken(@PathVariable("givenToken") Long givenToken, Model model)
+    /**
+     * Check the given token against the user in DB, if their token is matching,
+     * then authenticate the user (via removing the token and time instance from DB)
+     * otherwise, send the user back to the authentication page
+     * @param userId
+     * @param authenticationToken
+     * @param model
+     * @return home page if authenticated, otherwise authentication page
+     */
+    @PostMapping("/users/user/{userId}/authenticateEmail")
+    public String validateAuthenticationToken(@PathVariable("userId") Long userId,
+                                              @ModelAttribute("authenticationToken") String authenticationToken,
+                                              Model model) {
+        logger.info("authenticating token {} for user {}", authenticationToken, userId);
+
+        // check if token matches token in DB
+        GardenUser user = userService.getUserById(userId);
+        boolean authenticated = user.getEmailValidationToken().equals(authenticationToken);
+
+        logger.info("authentication: {}", authenticated);
+
+        if (authenticated) {
+            // remove token and time instant
+            user.setEmailValidationToken(null);
+            user.setEmailValidationTokenExpiryInstant(null);
+            userService.addUser(user);
+
+            return "redirect:/users/user";
+        }
+        else {
+            return "/authentication/emailAuthentication";
+        }
+    }
 }
