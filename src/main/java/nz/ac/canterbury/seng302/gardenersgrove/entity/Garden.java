a8438f514067852d802a19d0be0d63d7c8601233
tve21 <tve21@uclive.ac.nz>
2024-04-18 17:41:56 +0800

    Fix pipeline

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/Garden.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/Garden.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
+import jakarta.validation.constraints.Size;
 import nz.ac.canterbury.seng302.gardenersgrove.customValidation.ValidEuropeanDecimal;
 
 
@@ -27,6 +28,10 @@ public class Garden {
     @Pattern(regexp = "^$|^[A-Za-z0-9 .,'-]+$", message = "Location name must only include letters, numbers, spaces, dots, commas, hyphens or apostrophes")
     @Column(nullable = false)
     private String location;
+
+    @Size(max = 512, message = "Description must be 512 characters or less and contain some text")
+    @Pattern(regexp = "^.*[a-zA-Z].*|$", message = "Description must be 512 characters or less and contain some text")
+    private String description;
 
     @ValidEuropeanDecimal(message = "Garden size must be a positive number")
     @Column()
