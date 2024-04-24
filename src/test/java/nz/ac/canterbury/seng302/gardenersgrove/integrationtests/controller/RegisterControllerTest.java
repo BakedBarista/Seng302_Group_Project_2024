20501d7fb236e576c975c71eedeee90b7c782a18
Ryan Scofield <rsc104@uclive.ac.nz>
2024-04-24 18:38:41 +1200

    Fixed issues with PR
     - Fixed test failing without GardenUser.setId()

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integrationtests/controller/RegisterControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integrationtests/controller/RegisterControllerTest.java
@@ -7,6 +7,8 @@ import nz.ac.canterbury.seng302.gardenersgrove.service.TokenService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -22,22 +24,26 @@ public class RegisterControllerTest {
     @Autowired
     private TokenService tokenService;
 
+    @Spy
+    private GardenUser user;
+
+    private long id;
+
     @BeforeEach
     public void setUp() {
+        id = 1;
+        String firstName = "jane";
+        String lastName = "doe";
+        String email = "jane.doe@mail.com";
+        String password = "TESTPassword123!";
+        String dob = "01/01/2000";
+        user = new GardenUser(firstName, lastName, email, password, dob);
 
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
     public void testAddEmailTokenAndTimeToUser_AddsTokenAndTimeToUser() {
-        long id = 1;
-        String validFirstName = "jane";
-        String validLastName = "doe";
-        String validEmail = "jane.doe@mail.com";
-        String validPassword = "TESTPassword123!";
-        String validDOB = "01/01/2000";
-        GardenUser user = new GardenUser(validFirstName, validLastName, validEmail, validPassword, validDOB);
-        user.setId(id);
-
         // add user to persistence and then call function to add token and time instant
         gardenUserService.addUser(user);
         registerController.addEmailTokenAndTimeToUser(id);
