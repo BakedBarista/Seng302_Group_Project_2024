2796e47900231d17e7faa56dd9568afc4cacbb34
Luke Stynes <lst89@uclive.ac.nz>
2024-05-10 15:16:45 +1200

    fix: update the rest of the errors
    last commit missed this

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/dto/RegisterDTO.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/dto/RegisterDTO.java
@@ -1,12 +1,12 @@
 package nz.ac.canterbury.seng302.gardenersgrove.entity.dto;
 
-import static nz.ac.canterbury.seng302.gardenersgrove.customValidation.ValidationConstants.*;
-
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import nz.ac.canterbury.seng302.gardenersgrove.customValidation.AgeRange;
+
+import static nz.ac.canterbury.seng302.gardenersgrove.customValidation.ValidationConstants.*;
 
 /**
  * Data transfer object for the register form
@@ -18,7 +18,7 @@ public class RegisterDTO {
     @Column(nullable = false)
     private String fname;
 
-    @Pattern(regexp = NAME_REGEX, message = "Last name must only include letters, spaces,hyphens or apostrophes")
+    @Pattern(regexp = NAME_REGEX, message = "Last name must only include letters, spaces, hyphens or apostrophes")
     @Size(min = 0, max = NAME_MAX_LEN, message = "Last Name must be 64 characters long or less.")
     @Column(nullable = true)
     private String lname;
@@ -28,7 +28,7 @@ public class RegisterDTO {
     @Pattern(regexp = EMAIL_REGEX, message = "Email address must be in the form ‘jane@doe.nz’")
     private String email;
 
-    @Pattern(regexp = PASSWORD_REGEX, message = "Your password must beat least 8 characters long and include at least one uppercase letter, one lowercase letter, one number and one special character")
+    @Pattern(regexp = PASSWORD_REGEX, message = "Your password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character")
     private String password;
 
     private String confirmPassword;
