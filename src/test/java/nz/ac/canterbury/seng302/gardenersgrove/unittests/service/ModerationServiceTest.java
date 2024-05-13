99baec508f5909a8fc47dcf2f744a259f161d8be
Benjamin Davies <bentendavies@gmail.com>
2024-05-13 20:13:38 +1200

    Fix ModerationServiceTest so that it tests the correct class

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/service/ModerationServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/service/ModerationServiceTest.java
@@ -3,17 +3,17 @@ import nz.ac.canterbury.seng302.gardenersgrove.service.ModerationService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 public class ModerationServiceTest {
 
-    @Mock
     private ModerationService moderationService;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
+
+        moderationService = new ModerationService();
     }
 
     @Test
