a45c3485ddd3f8d73ff9972670f11733a3ced435
Ryan Scofield <rsc104@uclive.ac.nz>
2024-05-13 18:59:29 +1200

    tested small change

---
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/service/ModerationServiceTest.java
@@ -0,0 +1,23 @@
+package nz.ac.canterbury.seng302.gardenersgrove.unittests.service;
+import nz.ac.canterbury.seng302.gardenersgrove.service.ModerationService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class ModerationServiceTest {
+
+    @Mock
+    private ModerationService moderationService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void givenEmptyBody_ReturnFalse() {
+        Assertions.assertFalse(moderationService.checkIfDescriptionIsFlagged(""));
+    }
+}
