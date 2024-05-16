d7f8abcb3816494ef21229d9d905914968d06887
tve21 <tve21@uclive.ac.nz>
2024-05-16 13:25:21 +1200

    Wrote some cucumber tests for the cancel button feature

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/U18CancelRequestFeature.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/U18CancelRequestFeature.java
@@ -1,5 +1,6 @@
 package nz.ac.canterbury.seng302.gardenersgrove.cucumber.step_definitions;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.mock;
 
 
 import java.util.List;
@@ -9,28 +10,58 @@ import io.cucumber.java.en.And;
 
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Friends;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.GardenUser;
+import nz.ac.canterbury.seng302.gardenersgrove.repository.FriendsRepository;
+import nz.ac.canterbury.seng302.gardenersgrove.repository.GardenUserRepository;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenUserService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.FriendService;
+
+
 
 
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-public class U18CancelRequestFeature {
-    private GardenUserService userService;
-    private FriendService friendService;
+import io.cucumber.java.BeforeAll;
 
-    private GardenUser currentUser;
-    private GardenUser friend;
+
+public class U18CancelRequestFeature {
+
+    private static FriendsRepository friendsRepository;
+
+    private static GardenUserRepository gardenUserRepository;
+    private static GardenUserService userService;
+    private static  FriendService friendService;
+    private GardenUser user;
+    private static Long loggedInUserId;
+
+    private static GardenUser friend = new GardenUser();
+
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before all");
+        gardenUserRepository = mock(GardenUserRepository.class);
+        friendsRepository = mock(FriendsRepository.class);
+        userService = new GardenUserService(gardenUserRepository);
+        friendService = new FriendService(friendsRepository);
+
+         loggedInUserId = 1L;
+
+        friend.setId(loggedInUserId);
+        friend.setEmail("logged.in@gmail.com");
+        friend.setFname("Current");
+        friend.setFname("User");
+    }
 
     @Given("I send a friend request to {string} {string}")
     public void i_send_a_friend_request_to(String fName, String lName) {
-        GardenUser currentUser = userService.getCurrentUser();
+         user = U2LogInFeature.user;
+
         GardenUser friend = new GardenUser(fName, lName, "john.doe@gmail.com", "password", "");
 
-        if(currentUser != null) {
-            Friends friendRequest = new Friends(currentUser, friend, "pending");
+        if(user != null) {
+            Friends friendRequest = new Friends(user, friend, "pending");
             friendService.save(friendRequest);
         } else {
             throw new IllegalArgumentException("No user found with username");
@@ -38,25 +69,22 @@ public class U18CancelRequestFeature {
     }
     @When("I click on the cancel friend request button")
     public void i_click_on_the_cancel_request_button() {
-        Friends friendship = friendService.getFriendship(currentUser.getId(),friend.getId());
+        Friends friendship = friendService.getFriendship(user.getId(),friend.getId());
         friendService.removeFriendship(friendship);
     }
     @Then("I should not see {string} {string} in my friends list")
     public void i_should_not_see_in_my_friends_list(String fname, String lname) {
-        List<GardenUser> friends = friendService.getAllFriends(currentUser.getId());
+        List<GardenUser> friends = friendService.getAllFriends(user.getId());
         boolean isFriendPresent = friends.stream()
                 .anyMatch(friend -> friend.getFname().equals(fname));
         assertFalse(isFriendPresent, "Friend should not be present in the list");
     }
-    @And("I should not see {string} {string} in my pending friend requests list")
+    @And("I should not see {string} {string}  in my pending friend requests list")
     public void i_should_not_see_in_my_pending_friend_requests_list(String fname, String lname) {
-        List<Friends> pendingRequests = friendService.getPendingRequests(currentUser.getId());
+        List<Friends> pendingRequests = friendService.getSentRequests(user.getId());
         boolean isFriendPresent = pendingRequests.stream()
                 .anyMatch(friend -> friend.getReceiver().getFname().equals(fname));
         assertFalse(isFriendPresent, "Friend should not be present in the list");
     }
-
-
-
 
 }
