1c62c531fb9bc3c649ea5c70b40c48a7718f33a3
Maxzi <mfr91@uclive.ac.nz>
2024-05-24 12:28:58 +1200

    prevents invalid tags from being saved to the database

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/customValidation/ValidationConstants.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/customValidation/ValidationConstants.java
@@ -11,4 +11,7 @@ public class ValidationConstants {
     public static final String NAME_REGEX = "^[\\p{L}\\s'-]*$";
 
     public static final String GARDEN_REGEX = "^[\\p{L}0-9 .,'-]*$";
+
+    public static final String TAG_REGEX = "^[a-zA-Z0-9\\s\\-_']+$";
+    public static final int TAG_MAX_LEN = 25;
 }
