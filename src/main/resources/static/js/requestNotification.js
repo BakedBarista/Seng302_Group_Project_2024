df566f4f1f86ccea9a7c495b08c664b1537a6cda
Imogen Keeling <ike24@uclive.ac.nz>
2024-05-27 19:34:14 +1200

    fixing sonarqube squawks

--- src/main/resources/static/js/requestNotification.js
+++
@@ -1,9 +0,0 @@
-function countReceivedRequests() {
-    const receivedRequestsTableBody = document.querySelector('#requests-tab tbody');
-
-    if (receivedRequestsTableBody) {
-        return receivedRequestsTableBody.querySelectorAll('tr').length;
-    } else {
-        return 0;
-    }
-}
