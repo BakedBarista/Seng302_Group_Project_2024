94eaa03c0e55f1ff914871f26739054c90e571a2
Carl Chen <zch66@uclive.ac.nz>
2024-05-28 17:35:51 +1200

    fix tests

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/gardens/GardenController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/gardens/GardenController.java
@@ -1,6 +1,7 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller.gardens;
 
 
+import jakarta.annotation.PostConstruct;
 import jakarta.validation.Valid;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Friends;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
@@ -11,6 +12,7 @@ import nz.ac.canterbury.seng302.gardenersgrove.service.weatherAPI.WeatherAPIServ
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -21,6 +23,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import java.util.Arrays;
@@ -391,6 +396,65 @@ public class GardenController {
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * Create test data
+     * @throws IOException When problem reading file.
+     * ChatGPT help with processing sql queries to arraylist
+     */
+    @PostConstruct
+    public void dummyGardens() throws IOException {
+        try {
+            logger.info("Adding test data");
+
+            // Create user
+            GardenUser user = new GardenUser("Jan", "Doe", "jan.doe@gmail.com", "password", LocalDate.of(1970, 1, 1));
+            gardenUserService.addUser(user);
+
+            // Garden names
+            List<String> gardenNames = Arrays.asList(
+                    "Gardeners Paradise", "My Parents Garden", "Home Number 1", "GreenFingers", "Dirt Pile",
+                    "Potato Heaven", "Needs Weeding", "My Work in Progress", "Freshly Built", "Greener Pastures",
+                    "Grassy Grove", "Husbands Project", "Rainy Garden", "Tomato Garden", "Berries",
+                    "Roots", "Need a professional", "Growers Garden", "Community Garden", "Free for all Garden"
+            );
+
+            // Plant details
+            List<String[]> plantsDetails = Arrays.asList(
+                    new String[][]{
+                            {"Tomato", "Red"}, {"Cucumber", "Yellow"}, {"Potato", "Purple"},
+                            {"Cabbage", "Pink"}, {"Lettuce", "White"}, {"Onion", "Orange"},
+                            {"Spring Onion", "Blue"}, {"Asparagus", "Green"}, {"Pumpkin", "Purple"},
+                            {"Carrot", "Red"}
+                    }
+            );
+
+            for (int i = 0; i < gardenNames.size(); i++) {
+                String gardenName = gardenNames.get(i);
+                String streetNumber = Integer.toString(i + 1);
+                Garden garden = new Garden(gardenName, streetNumber, "Ilam Road", "Ilam", "Christchurch", "New Zealand", "8041", -43.5320, 172.6366, (String.valueOf(1000 + (i * 50))), "Test Garden");
+                garden.setOwner(user);
+                garden.setPublic(true);
+                gardenService.addGarden(garden);
+
+                List<Plant> plants = new ArrayList<>();
+                for (int j = 0; j < plantsDetails.size(); j++) {
+                    String[] plantDetail = plantsDetails.get(j);
+                    String plantName = plantDetail[0];
+                    String plantDescription = plantDetail[1];
+                    Plant plant = new Plant(plantName, "15", plantDescription, LocalDate.of(2024, 3, 1));
+                    Plant savedPlant = plantService.addPlant(plant, garden.getId());
+
+                    plants.add(savedPlant);
+                }
+
+                garden.setPlants(plants);
+                gardenService.addGarden(garden);
+            }
+        } catch (Exception e) {
+            logger.info("Failed to add garden");
         }
     }
 }
