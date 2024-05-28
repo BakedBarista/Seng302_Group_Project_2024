53ce63a37aa8921315c7efe6cddd36abcda073fa
Luke Stynes <lst89@uclive.ac.nz>
2024-05-28 19:45:43 +1200

    fix: remove unused locationapi code

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/LocationAPIController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/LocationAPIController.java
@@ -25,7 +25,6 @@ public class LocationAPIController {
     @Value("${geoapify.api.key}")
     private String location_apiKey;
 
-
     /**
      * Return result from the location API request
      *
@@ -50,51 +49,4 @@ public class LocationAPIController {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForEntity(url, String.class);
     }
-
-
-    /**
-     * Requests the API for the geocoding information for a manually inputted address
-     * @param location the manual address as a string
-     * @return location information in JSON
-     */
-    @GetMapping("/location-search")
-    public ResponseEntity<String> getManualLocationData(@RequestParam String location) {
-        logger.info("Requesting API for location match");
-        try {
-            Thread.sleep(300);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-        ResponseEntity<String> request = requestAPI(location, 1);
-        logger.info("API: {}", request.getBody());
-        return request;
-    }
-
-    /**
-     * Requests the location API.
-     * @param location the location to request
-     * @param limit the number of results to return
-     * @return the API response
-     */
-    private ResponseEntity<String> requestAPI(String location, int limit) {
-        try {
-            String url = "https://api.geoapify.com/v1/geocode/autocomplete"
-                    + "?text=" + URLEncoder.encode(location, StandardCharsets.UTF_8)
-                    + "&format=json"
-                    + "&limit=" + limit
-                    + "&apiKey=" + location_apiKey;
-
-            RestTemplate restTemplate = new RestTemplate();
-            return restTemplate.getForEntity(url, String.class);
-        } catch (HttpClientErrorException e) {
-             if (e.getStatusCode().value() == 401) {
-                logger.error("Authentication issue with location API, check API key.");
-            } else {
-                logger.error("An unknown error occurred with the location API.", e);
-            }
-        }
-        return null;
-    }
 }
-
-
