3ddb607e414728e35af4502fabc03d746d9597aa
Luke Stynes <lst89@uclive.ac.nz>
2024-05-29 10:27:04 +1200

    test: test the location service

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/service/LocationServiceTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/service/LocationServiceTest.java
@@ -1,43 +1,62 @@
 package nz.ac.canterbury.seng302.gardenersgrove.unittests.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nz.ac.canterbury.seng302.gardenersgrove.service.LocationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
-public class LocationService {
-    @Autowired
-    private GeoapifyService geoapifyService;
-
-    @MockBean
+class LocationServiceTest {
+    private LocationService locationService;
     private RestTemplate restTemplate;
-
-    @MockBean
     private ObjectMapper objectMapper;
 
-    @Value("${geoapify.api.key}")
-    private String locationApiKey;
+    @BeforeEach
+    public void setUp() {
+        restTemplate = Mockito.mock(RestTemplate.class);
+        objectMapper = Mockito.mock(ObjectMapper.class);
+        locationService = new LocationService(restTemplate, objectMapper);
+    }
 
     @Test
-    public void testGetLatLng() throws Exception {
-        String location = "Some location";
-        String url = "https://api.geoapify.com/v1/geocode/autocomplete"
-                + "?text=" + URLEncoder.encode(location, StandardCharsets.UTF_8)
-                + "&format=json"
-                + "&limit=1"
-                + "&apiKey=" + locationApiKey;
+    void GetLatLng_ValidLocation_ReturnsLatLng() throws Exception {
+        String location = "2 Janet Street Christchurch 8041 New Zealand";
 
         String jsonResponse = "{\"results\":[{\"lat\":-43.5320,\"lon\":172.6366}]}";
         ResponseEntity<String> responseEntity = new ResponseEntity<>(jsonResponse, HttpStatus.OK);
 
-        when(restTemplate.getForEntity(url, String.class)).thenReturn(responseEntity);
+        when(restTemplate.getForEntity(anyString(), eq(String.class))).thenReturn(responseEntity);
         when(objectMapper.readTree(jsonResponse)).thenReturn(new ObjectMapper().readTree(jsonResponse));
 
-        ArrayList<Double> latAndLng = geoapifyService.getLatLng(location);
+        List<Double> latAndLng = locationService.getLatLng(location);
 
         assertEquals(-43.5320, latAndLng.get(0), 0);
         assertEquals(172.6366, latAndLng.get(1), 0);
+    }
+
+    @Test
+    void GetLatLng_InvalidLocation_ReturnsEmptyList() throws Exception {
+        String location = "askdfjhaksjdhf";
+
+        String jsonResponse = "{\"results\":[]}";
+        ResponseEntity<String> responseEntity = new ResponseEntity<>(jsonResponse, HttpStatus.OK);
+
+        when(restTemplate.getForEntity(anyString(), eq(String.class))).thenReturn(responseEntity);
+        when(objectMapper.readTree(jsonResponse)).thenReturn(new ObjectMapper().readTree(jsonResponse));
+
+        List<Double> latAndLng = locationService.getLatLng(location);
+
+        assertTrue(latAndLng.isEmpty());
     }
 }
