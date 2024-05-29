f53c8235b822f5da245b5fe7b2b780acc025e840
Benjamin Davies <bentendavies@gmail.com>
2024-05-29 17:34:01 +1200

    Refactor common weather fields to shared superclass

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/weather/CurrentWeather.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/weather/CurrentWeather.java
@@ -5,15 +5,8 @@ import jakarta.persistence.Embeddable;
 import java.time.LocalDate;
 
 @Embeddable
-public class CurrentWeather {
-    private String city;
+public class CurrentWeather extends BaseWeather {
     private double temp;
-    private int humidity;
-    private String conditions;
-    private String icon;
-    private float windSpeed;
-    private int uv;
-
     private LocalDate date;
 
     public LocalDate getDate() {
@@ -24,59 +17,11 @@ public class CurrentWeather {
         this.date = date;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public double getTemp() {
         return temp;
     }
 
     public void setTemp(double temp) {
         this.temp = temp;
-    }
-
-    public int getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(int humidity) {
-        this.humidity = humidity;
-    }
-
-    public String getConditions() {
-        return conditions;
-    }
-
-    public void setConditions(String conditions) {
-        this.conditions = conditions;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-
-    public float getWindSpeed() {
-        return windSpeed;
-    }
-
-    public void setWindSpeed(float windSpeed) {
-        this.windSpeed = windSpeed;
-    }
-
-    public int getUv() {
-        return uv;
-    }
-
-    public void setUv(int uv) {
-        this.uv = uv;
     }
 }
--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/weather/WeatherData.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/weather/WeatherData.java
@@ -8,27 +8,12 @@ import java.time.LocalDate;
  * Creates an embeddable object for the weather data
  */
 @Embeddable
-public class WeatherData {
-
-    private String city;
+public class WeatherData extends BaseWeather {
     private LocalDate date;
     private double maxTemp;
     private double minTemp;
-    private int humidity;
-    private String conditions;
-    private String icon;
-    private float windSpeed;
     private double precipitation;
-    private int uv;
     private String type;
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
 
     public LocalDate getDate() {
         return date;
@@ -54,52 +39,12 @@ public class WeatherData {
         this.minTemp = minTemp;
     }
 
-    public int getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(int humidity) {
-        this.humidity = humidity;
-    }
-
-    public String getConditions() {
-        return conditions;
-    }
-
-    public void setConditions(String conditions) {
-        this.conditions = conditions;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-
-    public float getWindSpeed() {
-        return windSpeed;
-    }
-
-    public void setWindSpeed(float windSpeed) {
-        this.windSpeed = windSpeed;
-    }
-
     public double getPrecipitation() {
         return precipitation;
     }
 
     public void setPrecipitation(double precipitation) {
         this.precipitation = precipitation;
-    }
-
-    public int getUv() {
-        return uv;
-    }
-
-    public void setUv(int uv) {
-        this.uv = uv;
     }
 
     public String getType() {
