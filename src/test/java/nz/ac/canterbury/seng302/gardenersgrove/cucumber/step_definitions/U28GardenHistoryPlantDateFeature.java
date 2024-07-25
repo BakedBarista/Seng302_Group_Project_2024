903693d24b241eee9469d33a74953c1510d1cc37
Carl Chen <zch66@uclive.ac.nz>
2024-07-25 14:49:26 +1200

    Created step definition files for ACs that are related and added some placeholder definitions to be implemented.

---
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/U28GardenHistoryPlantDateFeature.java
@@ -0,0 +1,45 @@
+package nz.ac.canterbury.seng302.gardenersgrove.cucumber.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class U28GardenHistoryPlantDateFeature {
+    @Given("I have a garden {string}")
+    public void i_have_a_garden(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("I am on the add plant form")
+    public void i_am_on_the_add_plant_form() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I enter a valid plant name {string} and a valid date {string}")
+    public void i_enter_a_valid_plant_name_and_a_valid_date(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I submit the add plant form")
+    public void i_submit_the_add_plant_form() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the plant is successfully added")
+    public void the_plant_is_successfully_added() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I enter a valid plant name {string} and a invalid date {string}")
+    public void i_enter_a_valid_plant_name_and_a_invalid_date(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the plant is not added")
+    public void the_plant_is_not_added() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
