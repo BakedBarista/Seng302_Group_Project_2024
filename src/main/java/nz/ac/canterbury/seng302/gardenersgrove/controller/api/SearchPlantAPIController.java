9b3cd26c53ef66258dacdb1cf4ef3960e5570a40
Imogen Keeling <ike24@uclive.ac.nz>
2024-07-30 13:56:42 +1200

    added a controller to start autocomplete setup

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/api/SearchPlantAPIController.java
@@ -0,0 +1,33 @@
+package nz.ac.canterbury.seng302.gardenersgrove.controller.api;
+
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Plant;
+import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
+import nz.ac.canterbury.seng302.gardenersgrove.service.GardenUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class SearchPlantAPIController {
+
+    final Logger logger = LoggerFactory.getLogger(SearchPlantAPIController.class);
+    private GardenService gardenService;
+    private GardenUserService gardenUserService;
+
+//    private PlantAPIResults plantAPIResults;
+
+    public SearchPlantAPIController(GardenService gardenService, GardenUserService gardenUserService) {
+        this.gardenService = gardenService;
+        this.gardenUserService = gardenUserService;
+    }
+
+
+
+}
