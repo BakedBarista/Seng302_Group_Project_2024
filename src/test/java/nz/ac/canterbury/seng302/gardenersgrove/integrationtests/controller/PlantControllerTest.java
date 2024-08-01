ace062fe77b15fc476711f3ab9d83e2d4a718ee5
Luke Stynes <lst89@uclive.ac.nz>
2024-08-01 16:18:05 +1200

    fix some tests

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integrationtests/controller/PlantControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/integrationtests/controller/PlantControllerTest.java
@@ -11,15 +11,14 @@ import nz.ac.canterbury.seng302.gardenersgrove.repository.GardenUserRepository;
 import nz.ac.canterbury.seng302.gardenersgrove.repository.PlantRepository;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenUserService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.PlantHistoryService;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.security.core.Authentication;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 
@@ -27,7 +26,6 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -226,7 +224,7 @@ public class PlantControllerTest {
         Model model = Mockito.mock(Model.class);
         GardenUser notOwner = new GardenUser();
         when(userService.getCurrentUser()).thenReturn(notOwner);
-        String result = plantController.getPlantDetail(testGarden.getId(),testPlant.getId(),model);
+        String result = plantController.getPlantTimeline(testGarden.getId(),testPlant.getId(),model);
         Assertions.assertEquals("/error/accessDenied",result);
     }
 
