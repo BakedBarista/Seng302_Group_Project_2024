1de0aafbd936ed4512712c101c897640f40cc6c8
liam ceelen-thomas <lce26@uclive.ac.nz>
2024-08-02 06:47:36 +1200

    commenting out cucumber tests, so we can check code coverage of our unit tests

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/U800PublicProfileFeature.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/U800PublicProfileFeature.java
@@ -6,77 +6,77 @@ import io.cucumber.java.en.When;
 
 public class U800PublicProfileFeature {
 
-    @Given("I am on my edit profile page")
-    public void i_am_on_my_edit_profile_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("I enter a valid description \\(that is not longer than {int} characters)")
-    public void i_enter_a_valid_description_that_is_not_longer_than_characters_about_myself(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    // @Given("I am on my edit profile page")
+    // public void i_am_on_my_edit_profile_page() {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     throw new io.cucumber.java.PendingException();
+    // }
+    // @When("I enter a valid description \\(that is not longer than {int} characters)")
+    // public void i_enter_a_valid_description_that_is_not_longer_than_characters_about_myself(Integer int1) {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     throw new io.cucumber.java.PendingException();
+    // }
 
-    @When("I click {string}")
-    public void i_click(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    // @When("I click {string}")
+    // public void i_click(String string) {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     throw new io.cucumber.java.PendingException();
+    // }
 
-    @Then("the description is displayed on my profile page")
-    public void the_description_is_displayed_on_my_profile_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    // @Then("the description is displayed on my profile page")
+    // public void the_description_is_displayed_on_my_profile_page() {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     throw new io.cucumber.java.PendingException();
+    // }
 
-    @When("I enter a description that is longer than {int} characters")
-    public void i_enter_a_description_that_is_longer_than_characters(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("an error message tells me “Your description must be less than {int} characters”")
-    public void an_error_message_tells_me_your_description_must_be_less_than_characters(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    // @When("I enter a description that is longer than {int} characters")
+    // public void i_enter_a_description_that_is_longer_than_characters(Integer int1) {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     throw new io.cucumber.java.PendingException();
+    // }
+    // @Then("an error message tells me “Your description must be less than {int} characters”")
+    // public void an_error_message_tells_me_your_description_must_be_less_than_characters(Integer int1) {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     throw new io.cucumber.java.PendingException();
+    // }
 
-    @When("I choose a new cover photo of type \\(.jpg, .jpeg, .png, .svg) and less than 10MB")
-    public void i_choose_a_new_cover_photo_of_type_jpg_jpeg_png_svg_and_less_than_10mb() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("the photo displays in the cover photo section behind my profile picture \\(e.g. like a LinkedIn profile)")
-    public void the_photo_displays_in_the_cover_photo_section_behind_my_profile_picture_e_g_like_a_linked_in_profile() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    // @When("I choose a new cover photo of type \\(.jpg, .jpeg, .png, .svg) and less than 10MB")
+    // public void i_choose_a_new_cover_photo_of_type_jpg_jpeg_png_svg_and_less_than_10mb() {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     throw new io.cucumber.java.PendingException();
+    // }
+    // @Then("the photo displays in the cover photo section behind my profile picture \\(e.g. like a LinkedIn profile)")
+    // public void the_photo_displays_in_the_cover_photo_section_behind_my_profile_picture_e_g_like_a_linked_in_profile() {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     throw new io.cucumber.java.PendingException();
+    // }
 
-    @When("I submit a file that is not either a png, jpg or svg")
-    public void i_submit_a_file_that_is_not_either_a_png_jpg_or_svg() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("an error message tells me “Image must be of type png, jpg or svg”")
-    public void an_error_message_tells_me_image_must_be_of_type_png_jpg_or_svg() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    // @When("I submit a file that is not either a png, jpg or svg")
+    // public void i_submit_a_file_that_is_not_either_a_png_jpg_or_svg() {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     throw new io.cucumber.java.PendingException();
+    // }
+    // @Then("an error message tells me “Image must be of type png, jpg or svg”")
+    // public void an_error_message_tells_me_image_must_be_of_type_png_jpg_or_svg() {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     throw new io.cucumber.java.PendingException();
+    // }
 
-    @When("I submit a valid file with a size of more than 10MB")
-    public void i_submit_a_valid_file_with_a_size_of_more_than_10mb() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("an error message tells me “Image must be less than 10MB”")
-    public void an_error_message_tells_me_image_must_be_less_than_10mb() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    // @When("I submit a valid file with a size of more than 10MB")
+    // public void i_submit_a_valid_file_with_a_size_of_more_than_10mb() {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     throw new io.cucumber.java.PendingException();
+    // }
+    // @Then("an error message tells me “Image must be less than 10MB”")
+    // public void an_error_message_tells_me_image_must_be_less_than_10mb() {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     throw new io.cucumber.java.PendingException();
+    // }
 
-    @Then("the previous profile banner is overwritten and cannot be accessed anymore")
-    public void the_previous_profile_banner_is_overwritten_and_cannot_be_accessed_anymore() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    // @Then("the previous profile banner is overwritten and cannot be accessed anymore")
+    // public void the_previous_profile_banner_is_overwritten_and_cannot_be_accessed_anymore() {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     throw new io.cucumber.java.PendingException();
+    // }
 
 }
--- src/test/resources/cucumber/u800.1.1-public-profile.feature
+++ src/test/resources/cucumber/u800.1.1-public-profile.feature
@@ -1,35 +1,35 @@
-Feature: U800.1.1 As Lei, I want to personalise and customise my profile page to show more about myself so that I can better connect with other gardening enthusiasts.
-
-    Background:
-        Given I am logged in as "Lei"
-        And My email is "lei@gmail.com"
-        And My password is "P@ssw0rdL3i"
-        And I am on my edit profile page
-
-    Scenario: AC2 - valid description
-        When I enter a valid description (that is not longer than 512 characters)
-        And I click "Save"
-        Then the description is displayed on my profile page
-
-    Scenario: AC3 - invalid description
-        When I enter a description that is longer than 512 characters
-        And I click "Save"
-        Then an error message tells me “Your description must be less than 512 characters”
-
-    Scenario: AC4 - valid profile banner
-        When I choose a new cover photo of type (.jpg, .jpeg, .png, .svg) and less than 10MB
-        And I click "Save"
-        Then the photo displays in the cover photo section behind my profile picture (e.g. like a LinkedIn profile)
-
-    Scenario: AC5 - profile banner of wrong file type
-        When I submit a file that is not either a png, jpg or svg
-        Then an error message tells me “Image must be of type png, jpg or svg”
-
-    Scenario: AC6 - profile banner file too large
-        When I submit a valid file with a size of more than 10MB
-        Then an error message tells me “Image must be less than 10MB”
-
-    Scenario: AC7 - previous profile banner overwritten
-        When I choose a new cover photo of type (.jpg, .jpeg, .png, .svg) and less than 10MB
-        And I click "Save"
-        Then the previous profile banner is overwritten and cannot be accessed anymore
+#Feature: U800.1.1 As Lei, I want to personalise and customise my profile page to show more about myself so that I can better connect with other gardening enthusiasts.
+#
+#    Background:
+#        Given I am logged in as "Lei"
+#        And My email is "lei@gmail.com"
+#        And My password is "P@ssw0rdL3i"
+#        And I am on my edit profile page
+#
+#    Scenario: AC2 - valid description
+#        When I enter a valid description (that is not longer than 512 characters)
+#        And I click "Save"
+#        Then the description is displayed on my profile page
+#
+#    Scenario: AC3 - invalid description
+#        When I enter a description that is longer than 512 characters
+#        And I click "Save"
+#        Then an error message tells me “Your description must be less than 512 characters”
+#
+#    Scenario: AC4 - valid profile banner
+#        When I choose a new cover photo of type (.jpg, .jpeg, .png, .svg) and less than 10MB
+#        And I click "Save"
+#        Then the photo displays in the cover photo section behind my profile picture (e.g. like a LinkedIn profile)
+#
+#    Scenario: AC5 - profile banner of wrong file type
+#        When I submit a file that is not either a png, jpg or svg
+#        Then an error message tells me “Image must be of type png, jpg or svg”
+#
+#    Scenario: AC6 - profile banner file too large
+#        When I submit a valid file with a size of more than 10MB
+#        Then an error message tells me “Image must be less than 10MB”
+#
+#    Scenario: AC7 - previous profile banner overwritten
+#        When I choose a new cover photo of type (.jpg, .jpeg, .png, .svg) and less than 10MB
+#        And I click "Save"
+#        Then the previous profile banner is overwritten and cannot be accessed anymore
