9c52ddc36a505d7012fdd9c6fd6ee232dd29013b
Luke Stynes <lst89@uclive.ac.nz>
2024-08-07 17:16:35 +1200

    test: fix the tests

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/U8CreateNewGardenFeature.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/cucumber/step_definitions/U8CreateNewGardenFeature.java
@@ -79,7 +79,7 @@ public class U8CreateNewGardenFeature {
         moderationService = new ModerationService();
         mockedModerationService = mock(ModerationService.class);
         locationService = mock(LocationService.class);
-        gardenController = new GardenController(gardenService, null, plantService, userService, weatherAPIService, tagService, friendService, moderationService, profanityService, locationService);
+        gardenController = new GardenController(gardenService, null, plantService, userService, weatherAPIService, friendService, moderationService, profanityService, locationService);
     }
 
 
