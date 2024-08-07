dd091aa64d87234786e805f63a48b920c52e625c
Luke Stynes <lst89@uclive.ac.nz>
2024-08-07 14:12:27 +1200

    fix: update error message for plant timeline description

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/dto/PlantHistoryItemDTO.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/dto/PlantHistoryItemDTO.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 import static nz.ac.canterbury.seng302.gardenersgrove.validation.ValidationConstants.DESCRIPTION_REGEX;
-import static nz.ac.canterbury.seng302.gardenersgrove.validation.ValidationMessages.INVALID_GARDEN_DESCRIPTION;
+import static nz.ac.canterbury.seng302.gardenersgrove.validation.ValidationMessages.INVALID_PLANT_RECORD_DESCRIPTION;
 
 /**
  * Data transfer object for the plant history form
@@ -12,8 +12,8 @@ import static nz.ac.canterbury.seng302.gardenersgrove.validation.ValidationMessa
 public class PlantHistoryItemDTO {
 
 
-    @Size(max = 512, message = INVALID_GARDEN_DESCRIPTION)
-    @Pattern(regexp = DESCRIPTION_REGEX , message = INVALID_GARDEN_DESCRIPTION)
+    @Size(max = 512, message = INVALID_PLANT_RECORD_DESCRIPTION)
+    @Pattern(regexp = DESCRIPTION_REGEX , message = INVALID_PLANT_RECORD_DESCRIPTION)
     private String description;
 
 
