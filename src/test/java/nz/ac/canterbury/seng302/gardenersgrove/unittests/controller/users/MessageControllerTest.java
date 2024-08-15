3ee7210add36507e46927afdda8ec742565b9803
Carl Chen <zch66@uclive.ac.nz>
2024-08-15 14:18:24 +1200

    Basic unit test for controller and cucumber test

---
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/controller/users/MessageControllerTest.java
@@ -0,0 +1,24 @@
+package nz.ac.canterbury.seng302.gardenersgrove.unittests.controller.users;
+
+import nz.ac.canterbury.seng302.gardenersgrove.controller.users.MessageController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
+
+public class MessageControllerTest {
+
+    private MessageController messageController;
+    private Model model;
+
+    @BeforeEach
+    void setup() {
+        messageController = new MessageController();
+        model = null;
+    }
+    @Test
+    void whenClickMessageButton_thenReturnMessagePage() {
+        String result = messageController.messageFriend(1L,model);
+        Assertions.assertEquals("users/message", result);
+    }
+}
