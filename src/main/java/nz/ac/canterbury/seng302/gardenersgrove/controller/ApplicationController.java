6a8188dc51d6e45af3257fa80c384ff0c7fecc99
liam ceelen-thomas <lce26@uclive.ac.nz>
2024-08-21 07:08:42 +1200

    Added some basic funtionality to a accept button

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ApplicationController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/ApplicationController.java
@@ -1,15 +1,20 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller;
 
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Friends;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.GardenUser;
+import nz.ac.canterbury.seng302.gardenersgrove.service.FriendService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,11 +27,13 @@ public class ApplicationController {
 
     private GardenService gardenService;
     private GardenUserService gardenUserService;
+    private FriendService friendService;
 
     @Autowired
-    public ApplicationController(GardenService gardenService, GardenUserService gardenUserService) {
+    public ApplicationController(GardenService gardenService, GardenUserService gardenUserService,  FriendService friendService) {
         this.gardenService = gardenService;
         this.gardenUserService = gardenUserService;
+        this.friendService = friendService;
     }
 
     /**
@@ -48,6 +55,60 @@ public class ApplicationController {
         }
         return "home";
     }
+
+    /**
+     * Controls the initial home page controller when navigating to '/'
+     * @return the home page
+     */
+    @PostMapping("/")
+    public String homeAceept(@RequestParam(name = "action") String action, 
+        @RequestParam(name = "id") Long requestedId,  
+        Authentication authentication,
+        Model model) {
+
+        logger.info("Post /");
+
+        Long loggedInUserId = (Long) authentication.getPrincipal();
+        GardenUser loggedInUser = gardenUserService.getUserById(loggedInUserId);
+        GardenUser requestedUser = gardenUserService.getUserById(requestedId);
+
+        List<GardenUser> allUsers = gardenUserService.getUser();
+        List<GardenUser> friends = friendService.getAllFriends(loggedInUserId);
+        List<Friends> sentRequests = friendService.getSentRequests(loggedInUserId);
+        List<Friends> receivedRequests = friendService.getReceivedRequests(loggedInUserId);
+        
+        if ("accept".equals(action)) {
+            // accepting a already sent friend request
+            for (Friends receivedRequest : receivedRequests) {
+                if (receivedRequest.getSender().getId() == requestedId) {
+                    receivedRequest.setStatus(Friends.Status.ACCEPTED);
+                    friendService.save(receivedRequest);
+                    // return or do something here
+                    break;
+                }
+            }
+
+            boolean requestAlreadySent = false;
+        
+            // Check if a request has already been sent
+            for (Friends sentRequest : sentRequests) {
+                if (sentRequest.getReceiver().getId().equals(requestedId)) {
+                    requestAlreadySent = true;
+                    break;
+                }
+            }
+            
+            if(!requestAlreadySent){
+                //sending a new request to 
+                Friends newRequest = new Friends(loggedInUser, requestedUser, Friends.Status.PENDING);
+                friendService.save(newRequest);
+                logger.info("Friend request sent to " + requestedUser.getFullName());
+            }
+        }
+
+        return "home";
+    }
+
 
     @GetMapping("/ws-test")
     public String wsTest() {
