5eb59fd3eb9ded485b3cde9b91481486ed1ef3c3
Maxzi <mfr91@uclive.ac.nz>
2024-08-27 07:02:49 +1200

    controller mvc test

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/controller/PublicProfileControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/controller/PublicProfileControllerTest.java
@@ -1,7 +1,10 @@
 package nz.ac.canterbury.seng302.gardenersgrove.unittests.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nz.ac.canterbury.seng302.gardenersgrove.controller.users.PublicProfileController;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.GardenUser;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Plant;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.dto.EditUserDTO;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenUserService;
@@ -15,18 +18,37 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.core.Authentication;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.multipart.MultipartFile;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Assertions;
@@ -58,6 +80,8 @@ public class PublicProfileControllerTest {
 
     private EditUserDTO editUserDTO;
 
+    private static MockMvc mockMvc;
+
     @BeforeAll
     static void setup() {
         userId = 1L;
@@ -84,6 +108,9 @@ public class PublicProfileControllerTest {
         otherUser.setFname("Other");
         otherUser.setLname("User");
         otherUser.setDescription("This is a description for another user");
+
+        mockMvc = MockMvcBuilders.standaloneSetup(publicProfileController).build();
+
     }
 
     @ParameterizedTest
@@ -221,4 +248,33 @@ public class PublicProfileControllerTest {
 
         assertEquals("redirect:/users/public-profile", viewName);
     }
+
+    @Test
+    void givenISearchAPlant_whenPlantExists_thenResponseReturn() throws Exception {
+        String searchTerm = "Tomato";
+        Plant testPlant1 = new Plant("Tomato", "", "", LocalDate.of(1999, 10, 10));
+        Garden testGarden = new Garden("Garden", "1","Ilam Road","Ilam",
+                "Christchurch","New Zealand","8041",1.0,2.0, "Big", null);
+
+        given(plantService.getAllPlants(user, searchTerm)).willReturn(List.of(testPlant1));
+        ResponseEntity<List<Map<String, Object>>> result = publicProfileController.searchPlants(searchTerm);
+        System.out.println(result);
+        assertFalse(result.toString().isEmpty());
+
+        // post request
+        mockMvc.perform(post("http://localhost:8080/users/edit-public-profile/search")
+                .content(asJsonString(new ResponseEntity<List<Map<String, Object>>>(HttpStatusCode.valueOf(200))))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
