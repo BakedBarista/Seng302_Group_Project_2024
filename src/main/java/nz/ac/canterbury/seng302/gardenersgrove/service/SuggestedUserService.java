90fc8aa88e40d8b48e1fb8715a42e072d3a1b33f
Luke Stynes <lst89@uclive.ac.nz>
2024-08-27 19:36:06 +1200

    feat: create a SuggestedUserService

---
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/service/SuggestedUserService.java
@@ -0,0 +1,36 @@
+package nz.ac.canterbury.seng302.gardenersgrove.service;
+
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Friends;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SuggestedUserService {
+
+    private final FriendService friendService;
+
+    @Autowired
+    public SuggestedUserService(FriendService friendService) {
+        this.friendService = friendService;
+    }
+
+    /**
+     * Checks if there is an existing friendship request between two users.
+     * The requests can be either PENDING or DECLINED
+     * @param senderId the request sender's ID
+     * @param receiverId the request receiver's ID
+     * @return true if request exists, false otherwise
+     */
+    public boolean isRequestSent(Long senderId, Long receiverId) {
+        List<Friends> sentRequests = friendService.getSentRequests(senderId);
+
+        for (Friends sentRequest : sentRequests) {
+            if (sentRequest.getReceiver().getId().equals(receiverId)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
