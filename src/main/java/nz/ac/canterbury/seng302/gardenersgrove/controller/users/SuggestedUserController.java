80147f7ba7ab077cda7db91defc235ab7d3c4f3f
Luke Stynes <lst89@uclive.ac.nz>
2024-09-01 14:29:40 +1200

    feat: current state of controller

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/users/SuggestedUserController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/users/SuggestedUserController.java
@@ -1,6 +1,5 @@
 package nz.ac.canterbury.seng302.gardenersgrove.controller.users;
 
-import nz.ac.canterbury.seng302.gardenersgrove.entity.Friends;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.GardenUser;
 import nz.ac.canterbury.seng302.gardenersgrove.service.FriendService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenService;
@@ -77,35 +76,111 @@ public class SuggestedUserController {
     @PostMapping("/")
     public ResponseEntity<Map<String, Object>> homeAccept(
             @RequestParam(name = "action") String action,
-            @RequestParam(name = "id") Long requestedId,
+            @RequestParam(name = "suggestedId") Long suggestedId,
             Authentication authentication,
             Model model) {
         logger.info("Post /");
         Long loggedInUserId = (Long) authentication.getPrincipal();
         GardenUser loggedInUser = gardenUserService.getUserById(loggedInUserId);
-        GardenUser requestedUser = gardenUserService.getUserById(requestedId);
+        GardenUser suggestedUser = gardenUserService.getUserById(suggestedId);
+        Map<String, Object> response = new HashMap<>();
+        boolean doNothing = false;
 
-        boolean notAlreadyFriends = friendService.getAcceptedFriendship(loggedInUserId, requestedId) != null;
-        boolean requestedIdNotUser = !requestedId.equals(loggedInUserId);
 
-        Map<String, Object> response = new HashMap<>(); // TODO:
-        boolean success = false;
+        boolean requestedIdIsLoggedInUser = suggestedId.equals(loggedInUserId);
+        if (requestedIdIsLoggedInUser) {
+            logger.error("Suggested friend request was to logged in user's own ID. Doing nothing.");
+            doNothing = true;
+        }
 
-        if ("accept".equals(action) && requestedIdNotUser && notAlreadyFriends) {
-            logger.info("Accept button pressed by: {} on: {}", loggedInUserId, requestedId);
+        boolean alreadyFriends = friendService.getAcceptedFriendship(loggedInUserId, suggestedId) != null;
+        if (alreadyFriends) {
+            logger.error("Suggested friend and logged in user are already friends. Doing nothing");
+            doNothing = true;
+        }
 
-            success = suggestedUserService.attemptToAcceptPendingRequest(requestedId, loggedInUserId);
+        boolean alreadyDeclined = friendService.getDeclinedFriendship(loggedInUserId, suggestedId).isPresent();
+        if (alreadyDeclined) {
+            logger.error("Suggest friend and logged in user have a decline friendship. Doing nothing");
+            doNothing = true;
+        }
 
-            // If no request was accepted, check if a request was already sent
-            if (!success) {
-                boolean requestAlreadySent = suggestedUserService.friendRecordExists(loggedInUserId, requestedId);
+        // No more action to be taken if the above checks fail
+        if (doNothing) {
+            response.put("success", false);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
 
-                if (!requestAlreadySent) {
-                    Friends newRequest = new Friends(loggedInUser, requestedUser, Friends.Status.PENDING);
-                    friendService.save(newRequest);
+        // Accept button pressed flow
+        if ("accept".equals(action)) {
+            // Look for a pending request and accept it if one exists.
+            boolean pendingRequestAccepted = suggestedUserService.attemptToAcceptPendingRequest(suggestedId, loggedInUserId);
+            if (pendingRequestAccepted) {
+                logger.info("Pending request from suggested user accepted, user's are now friends");
+                response.put("success", true);
+            } else { // Send a new pending request to the suggested user
+                boolean newRequestSent = suggestedUserService.sendNewRequest(loggedInUser, suggestedUser);
+                if (!newRequestSent) {
+                    logger.error("Something went wrong trying to send a new request. Doing nothing");
+                    response.put("success", false);
+                    return ResponseEntity.status(HttpStatus.OK).body(response);
                 }
             }
         }
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+
+        // Decline button pressed flow
+//        if ("decline".equals(action)) {
+//            logger.info("Accept button pressed by: {} on: {}", loggedInUserId, suggestedId);
+//
+//            // Logged in user sent request to suggested user already then we do nothing.
+//
+//
+//
+//
+//            // If there is no friendship relation at all then we want to send a pending
+//
+//
+//        }
+//
+//
+//        // Do the accept button
+//        // Checks:
+//        // 1. Is there a pending friend request sent from the suggestedUser to us? Accept this
+//        // 2. Is there a pending friend request sent from us to the suggestedUser? Do nothing
+//        // 3. Is there already an accepted or decline status between us and the suggestedUser? Do nothing
+//        // 4. If there is no friend request at all then we want to send a request from us to the suggestedUser
+//
+//
+//
+//        // Do the decline button
+//        // 1. Is there a pending friend request sent from the suggestedUser to us? Remove this request and make it declined
+//        // 2. Is there a pending friend request sent from us to the suggestedUser? Decline their request
+//        // 3. Is there already an accepted or decline status between us and the suggestedUser? Do nothing
+//        // 4. If there is no friend request at all then we want to create a declined friendship from us to the suggestedUser
+//
+//
+//
+//
+//
+//        boolean success = false;
+//
+//
+//
+//        if ("accept".equals(action) && requestedIdNotUser && notAlreadyFriends) {
+//
+//
+//            // If no request was accepted, check if a request was already sent
+//            if (!success) {
+//                boolean requestAlreadySent = suggestedUserService.friendRecordExists(loggedInUserId, suggestedId);
+//
+//                if (!requestAlreadySent) {
+//                    Friends newRequest = new Friends(loggedInUser, suggestedUser, Friends.Status.PENDING);
+//                    friendService.save(newRequest);
+//                }
+//            }
+//        }
 //        if ("decline".equals(action) && !requestedId.equals(loggedInUserId) && alreadyFriends == null) {
 //            logger.info("Decline button pressed by: {} on: {}", loggedInUserId, requestedId);
 //
@@ -141,7 +216,6 @@ public class SuggestedUserController {
 
 
         // telling us when to trigger the toast, only when we have made a connection!
-        response.put("success", success);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+//        response.put("success", success);
     }
 }
