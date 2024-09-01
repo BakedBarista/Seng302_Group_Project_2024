9a01ce109e18caa5e8a7a0c31a57891cb5c1bf94
Luke Stynes <lst89@uclive.ac.nz>
2024-09-02 10:46:46 +1200

    refactor: set up a post handler for connection.js

--- src/main/resources/static/js/connection.js
+++ src/main/resources/static/js/connection.js
@@ -1,3 +1,8 @@
+const Actions = {
+    ACCEPT: 'accept',
+    DECLINE: 'decline',
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     let acceptButton = document.getElementById('heartButton');
     let declineButton = document.getElementById('declineButton');
@@ -7,63 +12,55 @@ document.addEventListener('DOMContentLoaded', function () {
     if (acceptButton) {
         acceptButton.addEventListener('click', function (event) {
             event.preventDefault();
-            let formData = new FormData();
-            formData.append('action', 'accept');
-            formData.append('id', 7); // this needs to be the id of user.
-
-            fetch(`${baseUrl}`, {
-                method: 'POST',
-                headers: {
-                    [csrfHeader]: csrf,
-                },
-                body: formData
-            })
-                .then(response => {
-                    console.log('Raw response:', response);
-                    if (response.ok) {
-                        return response.json();
-                    } else {
-                        throw new Error('Network response was not ok.');
-                    }
-                })
-                .then(data => {
-                    console.log('Response data:', data);
-                    if (data.success) {
-                        toast.show();
-                    } else {
-                        console.error('Request failed or no matching request found.');
-                    }
-                })
-                .catch(error => {
-                    console.error('There was a problem with the fetch operation:', error);
-                });
+            //TODO: This user ID needs to be updated in the new task
+            handlePost(Actions.ACCEPT, 7);
         });
     }
+
     if (declineButton) {
         declineButton.addEventListener('click', function (event) {
             event.preventDefault();
-            let formData = new FormData();
-            formData.append('action', 'decline');
-            formData.append('id', 7); // this needs to be the id of user.
-
-            fetch(`${baseUrl}`, {
-                method: 'POST',
-                headers: {
-                    [csrfHeader]: csrf,
-                },
-                body: formData
-            })
-                .then(response => {
-                    console.log('Raw response:', response);
-                    if (response.ok) {
-                        return response.json();
-                    } else {
-                        throw new Error('Network response was not ok.');
-                    }
-                })
-                .catch(error => {
-                    console.error('There was a problem with the fetch operation:', error);
-                });
+            //TODO: This user ID needs to be updated in the new task
+            handlePost(Actions.DECLINE, 7);
         });
     }
 });
+
+
+/**
+ * Handles the Post API request to the server for both accept and decline
+ * @param action either 'accept' or 'decline'
+ * @param suggestedUserId
+ */
+function handlePost(action, suggestedUserId) {
+    let formData = new FormData();
+    formData.append('action', action);
+    formData.append('id', suggestedUserId);
+
+    fetch(`${baseUrl}`, {
+        method: 'POST',
+        headers: {
+            [csrfHeader]: csrf,
+        },
+        body: formData
+    })
+    .then(response => {
+        console.log('Raw response:', response);
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Network response was not ok.');
+        }
+    })
+    .then(data => {
+        console.log('Response data:', data);
+        if (data.success) {
+            toast.show();
+        } else {
+            console.error('Request failed or no matching request found.');
+        }
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
