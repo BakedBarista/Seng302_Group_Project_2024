90a91d9e36927989e753a9ae71039214fd73dffa
Mfr91 <mfr91@uclive.ac.nz>
2024-09-10 15:15:22 +1200

    fix empty strings getting accepted

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/dto/MessageDTO.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/entity/dto/MessageDTO.java
@@ -1,6 +1,5 @@
 package nz.ac.canterbury.seng302.gardenersgrove.entity.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 /** DTO For handing the submission of messages */
