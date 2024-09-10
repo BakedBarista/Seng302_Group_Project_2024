b650f9c36e48fdee250f78270736e90f00dee701
Mfr91 <mfr91@uclive.ac.nz>
2024-09-10 14:50:34 +1200

    more code smell fix

--- src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/gardens/PlantController.java
+++ src/main/java/nz/ac/canterbury/seng302/gardenersgrove/controller/gardens/PlantController.java
@@ -7,7 +7,6 @@ import nz.ac.canterbury.seng302.gardenersgrove.entity.Garden;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.GardenUser;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Plant;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.PlantHistoryItem;
-import nz.ac.canterbury.seng302.gardenersgrove.entity.dto.GardenDTO;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.dto.PlantDTO;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.dto.PlantHistoryItemDTO;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.dto.PlantInfoDTO;
