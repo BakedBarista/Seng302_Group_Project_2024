49f2f38aad08447deea6d8464b2edb05f05ad8a8
Imogen Keeling <ike24@uclive.ac.nz>
2024-09-10 21:17:58 +1200

    wrote a unit test for MessageController. Not working yet

--- src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/controller/users/MessageControllerTest.java
+++ src/test/java/nz/ac/canterbury/seng302/gardenersgrove/unittests/controller/users/MessageControllerTest.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import nz.ac.canterbury.seng302.gardenersgrove.controller.users.MessageController;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.Friends;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.GardenUser;
+import nz.ac.canterbury.seng302.gardenersgrove.entity.Message;
 import nz.ac.canterbury.seng302.gardenersgrove.entity.dto.MessageDTO;
 import nz.ac.canterbury.seng302.gardenersgrove.service.FriendService;
 import nz.ac.canterbury.seng302.gardenersgrove.service.GardenUserService;
@@ -18,8 +19,14 @@ import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.core.Authentication;
 import org.springframework.ui.Model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import static nz.ac.canterbury.seng302.gardenersgrove.entity.Friends.Status.ACCEPTED;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 
@@ -37,6 +44,9 @@ class MessageControllerTest {
 
     private static HttpSession session;
 
+    private final Long loggedInUserId = 1L;
+    private final Long requestedUserId = 2L;
+
     @BeforeEach
     public void setup() {
         gardenUserService = mock(GardenUserService.class);
@@ -50,12 +60,12 @@ class MessageControllerTest {
 
     @Test
     void whenClickMessageButton_thenReturnMessagePage() {
-        Mockito.when(authentication.getPrincipal()).thenReturn(1L);
-        Mockito.when(mockedFriendService.getFriendship(any(), any())).thenReturn(new Friends());
-        Mockito.when(gardenUserService.getUserById(1L)).thenReturn(new GardenUser());
+        when(authentication.getPrincipal()).thenReturn(1L);
+        when(mockedFriendService.getFriendship(any(), any())).thenReturn(new Friends());
+        when(gardenUserService.getUserById(1L)).thenReturn(new GardenUser());
 
         String result = messageController.messageFriend(1L, authentication,  model,session);
-        Assertions.assertEquals("users/message-home", result);
+        assertEquals("users/message-home", result);
     }
 
     @Test
@@ -64,12 +74,12 @@ class MessageControllerTest {
         Long receiver = 2L;
         MessageDTO messageDTO = new MessageDTO("Hello","token");
 
-        Mockito.when(authentication.getPrincipal()).thenReturn(sender);
-        Mockito.when(mockedFriendService.getFriendship(any(), any())).thenReturn(new Friends());
-        Mockito.when(gardenUserService.getUserById(sender)).thenReturn(new GardenUser());
+        when(authentication.getPrincipal()).thenReturn(sender);
+        when(mockedFriendService.getFriendship(any(), any())).thenReturn(new Friends());
+        when(gardenUserService.getUserById(sender)).thenReturn(new GardenUser());
 
         String result = messageController.sendMessage(receiver, messageDTO, authentication,  model, session);
-        Assertions.assertEquals("users/message-home", result);
+        assertEquals("users/message-home", result);
     }
 
     @Test
@@ -78,11 +88,42 @@ class MessageControllerTest {
         Long receiver = 2L;
         MessageDTO messageDTO = new MessageDTO("Hello","token");
         session.setAttribute("submissionToken", "token");
-        Mockito.when(authentication.getPrincipal()).thenReturn(sender);
-        Mockito.when(mockedFriendService.getFriendship(any(), any())).thenReturn(new Friends());
-        Mockito.when(gardenUserService.getUserById(sender)).thenReturn(new GardenUser());
+        when(authentication.getPrincipal()).thenReturn(sender);
+        when(mockedFriendService.getFriendship(any(), any())).thenReturn(new Friends());
+        when(gardenUserService.getUserById(sender)).thenReturn(new GardenUser());
 
         messageController.sendMessage(receiver, messageDTO, authentication,  model,session);
-        Mockito.verify(mockedMessageService).sendMessage(sender, receiver, messageDTO);
+        verify(mockedMessageService).sendMessage(sender, receiver, messageDTO);
+    }
+
+
+    @Test
+    void whenMessageHomeGET_thenReturnMessageHome() {
+        GardenUser user1 = new GardenUser();
+        user1.setId(loggedInUserId);
+        GardenUser user2 = new GardenUser();
+        user2.setId(requestedUserId);
+
+        List<Message> allMessages = List.of(new Message(loggedInUserId, requestedUserId, LocalDateTime.now(), "Hey"));
+        Map<Long, Message> recentMessagesMap = Map.of(requestedUserId, allMessages.get(0));
+        Map<GardenUser, String> recentChats = Map.of(user2, "Hey");
+        Friends friend = new Friends(user1, user2, ACCEPTED);
+
+        when(mockedMessageService.findAllRecentChats(loggedInUserId)).thenReturn(allMessages);
+        when(mockedMessageService.getLatestMessages(allMessages, loggedInUserId)).thenReturn(recentMessagesMap);
+        when(mockedMessageService.convertToPreview(recentMessagesMap)).thenReturn(recentChats);
+        when(mockedMessageService.getActiveChat(recentMessagesMap)).thenReturn(requestedUserId);
+        when(gardenUserService.getUserById(requestedUserId)).thenReturn(user1);
+        when(mockedFriendService.getFriendship(loggedInUserId, requestedUserId)).thenReturn(friend);
+
+        // Act
+        String view = messageController.messageHome(authentication, model, session);
+
+        // Assert
+        assertEquals("users/message-home", view);
+        verify(model).addAttribute(eq("recentChats"), eq(recentChats));
+        verify(model).addAttribute(eq("sentToUser"), any(GardenUser.class));
+        verify(model).addAttribute(eq("activeChat"), eq(requestedUserId));
+        verify(model).addAttribute(eq("messagesMap"), any());
     }
 }
